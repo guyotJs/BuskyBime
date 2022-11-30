192cf0eab51e5d82dca6c8d6b08b12fe61c5572b
ClassicMC <classicmclive@gmail.com>
2022-11-30 04:54:09 +0000

    v0.0.1

---
+++ tuskytime/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import tt from './elements/tt.json';
+  import Header from './elements/Header.svelte';
+  let now = new Date(),year, month, day;
+  let dateString;
+  dateString = (now.getMonth()+1)+'/'+now.getDate();
+</script>
+<style>
+  :global(body){
+    background-color: #9db6f8;
+    margin:0;
+  }
+  .boing{
+    text-align: center;
+  }
+</style>
+<main>
+  <Header/>
+  <div class="boing">
+    <br/>
+    {dateString}<br/>
+    TODAY IS
+    {#if tt[dateString].day == "B"}
+      A
+    {:else}
+      AN
+    {/if}<br/>
+    {tt[dateString].day}<br/>
+    DAY<br/>
+    AND IT'S TUSKY TIME<br/>
+    {tt[dateString].TT}
+  </div>
+</main>
---
+++ tuskytime/src/elements/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+<style>
+    .bg{
+        background-color: #6882d3;
+        text-align: center;
+        padding: 10px;
+        border-bottom: 15px solid #25408f;
+        font-size:35px;
+    }
+</style>
+<main>
+    <div class="bg">
+        <b>TUSCARORA TIME</b>
+    </div>
+</main>
