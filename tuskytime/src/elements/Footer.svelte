394b82e3feb65d9849ffea9941ab1f2dd3d24ff3
Weston <classicmclive@gmail.com>
2024-05-02 15:30:52 -0400

    Update Footer.svelte

--- tuskytime/src/elements/Footer.svelte
+++ tuskytime/src/elements/Footer.svelte
@@ -8,6 +8,10 @@
         left:50%;
         transform:translate(-50%);
         margin:0;
+        background-color:#9db6f8;
+        width:100%;
+        text-align:center;
+        border:1px solid #6882d3;
     }
     p{
         color: #6882d3;
@@ -19,5 +23,5 @@
     }
 </style>
 <main>
-    <p class:foot={!mobile} class:boot={mobile}>Not affiliated with LCPS, An improved version of <a href="https://tuskytime.org">Tuskytime.org</a> by, Weston Schnee in <em>Svelte</em></p>
+    <p class:foot={!mobile} class:boot={mobile}><em>Not affiliated with LCPS</em>, An Simpler Version of <a href="https://tuskytime.org">Tuskytime</a> by, Weston Schnee in <em>Svelte</em></p>
 </main>
